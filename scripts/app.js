cdb9bdee4545f1b74a122bcee8037f2a06080437
Benjamin Chan <benchan_2000@hotmail.com>
2019-10-25 18:48:42 +0100

    Added selector examples

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,47 @@
-// Select the H1
-var h1 = document.querySelector("h1");
+/*
+  DOM EXAMPLE
 
-// Manipulate a property
-h1.style.color = "red";
+  // Select the H1
+  var h1 = document.querySelector("h1");
 
-// Select the body
-var body = document.querySelector("body");
-var isBlue = false;
+  // Manipulate a property
+  h1.style.color = "red";
 
-// Manipulate a property
-setInterval(function () {
+  // Select the body
+  var body = document.querySelector("body");
+  var isBlue = false;
+
+  // Manipulate a property
+  setInterval(function () {
   if (isBlue) {
     body.style.background = "white";
   } else {
     body.style.background = "#3498db";
   }
   isBlue = !isBlue;
-}, 2000);
+  }, 2000);
+
+*/
+
+/*
+  Selector Methods
+
+  document.getElementById()
+  document.getElementsByClassName()
+  document.getElementsByTagName()
+  document.querySelector()
+  document.querySelectorAll()
+
+*/
+
+var list1 = document.getElementById("highlight"); // Return the element with ID
+
+var tags = document.getElementsByClassName("bolded"); // Return a HTML Collection not a Array
+
+var allList = document.getElementsByTagName("li"); // Return a HTML Collection
+
+/* These to are the best Selectors to use */
+var listIDStyle = document.querySelector("#highlight"); // Return first element matching the CSS Style selector
+
+var allTagsStyle = document.querySelectorAll(".bolded"); // Return a HTML Collection with CSS Style selector
+
